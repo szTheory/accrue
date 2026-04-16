7a5036100966fc5f1249e18a12f2d2acabd4cb0a
szTheory <szTheory@users.noreply.github.com>
2026-04-16 12:32:29 -0400

    feat(10-host-app-dogfood-harness-02): add host auth scaffold
    - generate Phoenix auth/session boundary for AccrueHost
    - wire the host user schema into Accrue.Billable for later flows

--- examples/accrue_host/lib/accrue_host_web/router.ex
+++ examples/accrue_host/lib/accrue_host_web/router.ex
@@ -1,6 +1,8 @@
 defmodule AccrueHostWeb.Router do
   use AccrueHostWeb, :router
 
+  import AccrueHostWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule AccrueHostWeb.Router do
     plug :put_root_layout, html: {AccrueHostWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -27,11 +30,38 @@ defmodule AccrueHostWeb.Router do
 
   # Enable Swoosh mailbox preview in development
   if Application.compile_env(:accrue_host, :dev_routes) do
-
     scope "/dev" do
       pipe_through :browser
 
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", AccrueHostWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{AccrueHostWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", AccrueHostWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{AccrueHostWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
