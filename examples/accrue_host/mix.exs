7f7fc35f677f71e57f314657ba34d93be821f8f1
szTheory <szTheory@users.noreply.github.com>
2026-04-16 18:12:09 -0400

    feat(12-08): add host dependency mode switch
    - gate canonical host deps with ACCRUE_HOST_HEX_RELEASE
    - derive Hex versions from sibling package mix.exs files

--- examples/accrue_host/mix.exs
+++ examples/accrue_host/mix.exs
@@ -62,13 +62,48 @@ defmodule AccrueHost.MixProject do
       {:req, "~> 0.5"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:accrue, path: "../../accrue"},
-      {:accrue_admin, path: "../../accrue_admin"},
+      accrue_dep(),
+      accrue_admin_dep(),
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"}
     ]
+  end
+
+  defp accrue_dep do
+    if hex_release?() do
+      {:accrue, "~> #{accrue_version()}"}
+    else
+      {:accrue, path: "../../accrue"}
+    end
+  end
+
+  defp accrue_admin_dep do
+    if hex_release?() do
+      {:accrue_admin, "~> #{accrue_admin_version()}"}
+    else
+      {:accrue_admin, path: "../../accrue_admin"}
+    end
+  end
+
+  defp hex_release?, do: System.get_env("ACCRUE_HOST_HEX_RELEASE") == "1"
+
+  defp accrue_version, do: sibling_package_version!("../../accrue/mix.exs")
+
+  defp accrue_admin_version, do: sibling_package_version!("../../accrue_admin/mix.exs")
+
+  defp sibling_package_version!(relative_path) do
+    relative_path
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> case do
+      contents ->
+        case Regex.run(~r/@version\s+"([^"]+)"/, contents, capture: :all_but_first) do
+          [version] -> version
+          _ -> raise "could not parse @version from #{relative_path}"
+        end
+    end
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
